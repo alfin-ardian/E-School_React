2e83be3715f3c98854fb80528eb32fd4ac1ea5a5
alfin ardian <developer.alfin@gmail.com>
2023-05-02 08:49:26 +0700

    add pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const logoProps = {
+    width: 96,
+    height: 96,
+  };
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img
+            src="/vite.svg"
+            className="logo"
+            alt="Vite logo"
+            {...logoProps}
+          />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+            {...logoProps}
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +43,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
